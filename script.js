84b3003f6a42cb1e7800aa8a1324b163ed224753
yaz <g4xl@hotmail.com>
2024-09-24 04:12:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,3 @@ document.addEventListener("DOMContentLoaded", function() {
                 return warmColors[Math.floor(Math.random() * warmColors.length)];
             }
         });
-
-  
-/*   $(window).resize(function() {
-      if ($(window).width() < 768) {
-          $('.column-class').css('width', '100%');
-      } else {
-          $('.column-class').css('width', '50%');
-      }
-  }); 
