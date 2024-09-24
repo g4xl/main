1bef2485e06d81dafad3e98d3cb5cf31b722f3e6
yaz <g4xl@hotmail.com>
2024-09-24 03:16:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-  $(window).resize(function() {
+        document.addEventListener("DOMContentLoaded", function() {
+            const icons = document.querySelectorAll('.random-color');
+            icons.forEach(icon => {
+                icon.style.color = getRandomColor();
+            });
+            function getRandomColor() {
+                const letters = '0123456789ABCDEF';
+                let color = '#';
+                for (let i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+            }
+        });
+  
+/*   $(window).resize(function() {
       if ($(window).width() < 768) {
           $('.column-class').css('width', '100%');
       } else {
           $('.column-class').css('width', '50%');
       }
   });
-  
