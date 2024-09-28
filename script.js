41b5b9f1507e53a2e8b4f77bda4ee115b9796cc7
yaz <g4xl@hotmail.com>
2024-09-28 21:53:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const icons = document.querySelectorAll('.icon');
-    icons.forEach(icon => {
-        icon.style.color = getRandomWarmColor();
-    });
-
-    function getRandomWarmColor() {
-        const warmColors = [
-            '#C96868',  // طماطم
-            '#FADFA1',  // برتقالي قوي
-            '#FFF4EA',  // ذهبي
-            '#7EACB5',  // برتقالي داكن
-            '#CED89E',  // سمك السلمون الفاتح
-            '#FFDCAE',  // بني رملي
-            '#D2691E',  // شوكولاتة
-            '#76BA99'   // أحمر بني
-        ];
-        // اختيار لون عشوائي من القائمة
-        return warmColors[Math.floor(Math.random() * warmColors.length)];
-    }
-});
+        function getRandomWarmColor() {
+            const warmColors = [
+                '#C96868',  // طماطم
+                '#FADFA1',  // برتقالي قوي
+                '#FFF4EA',  // ذهبي
+                '#7EACB5',  // برتقالي داكن
+                '#CED89E',  // سمك السلمون الفاتح
+                '#FFDCAE',  // بني رملي
+                '#D2691E',  // شوكولاتة
+                '#76BA99'   // أحمر بني
+            ];
+            return warmColors[Math.floor(Math.random() * warmColors.length)];
+        }
+        document.addEventListener("DOMContentLoaded", function() {
+            const icons = document.querySelectorAll('.icon');
+            icons.forEach(icon => {
+                icon.style.color = getRandomWarmColor();
+            });
+        });
