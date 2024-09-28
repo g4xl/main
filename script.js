ba5b3f3ef129af393dd9c1ae476a015b7b390e55
yaz <g4xl@hotmail.com>
2024-09-28 21:28:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    const icon = this.querySelector('i');
-    if (document.body.classList.contains('dark-mode')) {
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
-    } else {
-        icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon');
-    }
-});
-document.addEventListener("DOMContentLoaded", function() {
-    // إضافة فئة الوضع الليلي كافتراضي
-    document.body.classList.add('dark-mode');
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     const icons = document.querySelectorAll('.icon');
     icons.forEach(icon => {
